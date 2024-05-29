6f529dde2273d484deb13888d8e81d494e8c3e71
noeee-jpg <hnoer4646@gmail.com>
2024-05-29 20:31:37 +0700

    chrone : designed base code fullstack

--- services/fullstack/main.py
+++ services/fullstack/main.py
@@ -3,14 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-<<<<<<< HEAD
 def index():
     return "<p>Selamat datang di Klinik Terpadu</p>"
 
 def login()
 
 def regriter()
-=======
-def hello_world():
-    return "<p>Hello, World!</p>"
->>>>>>> 671428f482041314d57a33303f390dfcdd4c50b1
